e3921d4b4f1981fae498db786eb8f9706acf66af
Adrian <mogekoff@gmail.com>
2021-11-14 05:30:42 +0500

    Добавил в конфиг кастом фигур

--- config.py
+++ config.py
@@ -2,3 +2,7 @@ w_width = 500
 w_height = 500
 ip = '127.0.0.1'
 port = 4444
+figures = {
+    'white': dict(pawn='♙', knight='♘', bishop='♗', rook='♖', queen='♕', king='♔', cell='◻'),
+    "black": dict(pawn='♟', knight='♞', bishop='♝', rook='♜', queen='♛', king='♚', cell='■')
+}
