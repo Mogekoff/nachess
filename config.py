c5e3382cfc337294f49d9376a4953f39a83236a1
Adrian <mogekoff@gmail.com>
2021-11-16 02:29:00 +0500

    Добавил кастомные нижние координаты

--- config.py
+++ config.py
@@ -4,5 +4,6 @@ ip = '127.0.0.1'
 port = 4444
 figures = {
     'white': dict(pawn='♙', knight='♘', bishop='♗', rook='♖', queen='♕', king='♔', cell='◻'),
-    "black": dict(pawn='♟', knight='♞', bishop='♝', rook='♜', queen='♛', king='♚', cell='■')
+    'black': dict(pawn='♟', knight='♞', bishop='♝', rook='♜', queen='♛', king='♚', cell='■'),
+    'coordinates': '0 1 2 3 4 5 6 7'  # or '0 1 2 3 4 5 6 7' or 'А Б В Г Д Е Ж З'
 }
