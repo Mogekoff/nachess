4d21e2ea30dc4ab4c48af59d414646cde5aaf5c1
Adrian <mogekoff@gmail.com>
2021-11-16 02:01:57 +0500

    Перенес запуск в main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from nachess import *
 import pygame
 
 def main():
-    pass
+    field = Field()
+    field.draw_classic()
+    field.print()
+    while True:
+        x, y, X, Y = int(input()), int(input()), int(input()), int(input())
+        field.field[x][y].move(X, Y)
+        field.print()
 
 
 if __name__ == '__main__':
